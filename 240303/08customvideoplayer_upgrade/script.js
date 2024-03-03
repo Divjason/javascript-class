6b1ca4df1195cc7d7c36670760d717b2ff2382ff
JSON XHR <126494515+Divjason@users.noreply.github.com>
2024-03-03 23:46:06 +0900

    Add files via upload

---
+++ 240303/08customvideoplayer_upgrade/script.js
@@ -0,0 +1,83 @@
+const player = document.querySelector(".player");
+const video = document.querySelector("video");
+const playButton = document.querySelector(".play-pause");
+const volumeBar = document.querySelector("input[type='range']");
+const rateButtons = document.querySelectorAll(".rate");
+const progressCover = document.querySelector(".progress");
+
+const videoPoint = (e) => {
+  const mouseX = e.pageX - player.offsetLeft;
+  const width = progressCover.clientWidth;
+  const duration = video.duration;
+  const clickedTime = (mouseX / width) * duration;
+  video.currentTime = clickedTime;
+};
+
+const updateProgress = () => {
+  const progressBar = document.querySelector(".bar");
+  const progressPointer = document.querySelector(".circle");
+  const currentTime = video.currentTime;
+  const duration = video.duration;
+  const percent = (currentTime / duration) * 100;
+  progressBar.style.width = `${percent}%`;
+  const progressBarWidth = progressCover.clientWidth;
+  const newPosition = (currentTime / duration) * progressBarWidth - 1;
+  progressPointer.style.left = `${newPosition}px`;
+};
+
+const setRate = (e) => {
+  const { rate } = e.target.dataset;
+  video.playbackRate = rate;
+};
+
+const formatiing = (time) => {
+  const sec = Math.floor(time % 60);
+  const min = Math.floor(time / 60);
+  const hour = Math.floor(time / 3600);
+
+  const fSec = sec < 10 ? `0${sec}` : sec;
+  const fMin = min < 10 ? `0${min}` : min;
+  const fHour = hour < 10 ? `0${hour}` : hour;
+
+  return `${fHour}:${fMin}:${fSec}`;
+};
+
+const updateTime = () => {
+  const current = document.querySelector(".current");
+  const duration = document.querySelector(".duration");
+
+  current.innerText = formatiing(video.currentTime);
+  duration.innerText = formatiing(video.duration);
+};
+
+const setVolume = (e) => {
+  video.volume = e.target.value;
+};
+
+const play = () => {
+  playButton.innerText = "∥";
+  video.play();
+};
+
+const pause = () => {
+  playButton.innerText = "▶";
+  video.pause();
+};
+
+const togglePlay = () => {
+  console.log("click");
+  video.paused ? play() : pause();
+};
+
+playButton.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+
+volumeBar.addEventListener("change", setVolume);
+video.addEventListener("timeupdate", updateTime);
+video.addEventListener("timeupdate", updateProgress);
+
+rateButtons.forEach((button) => {
+  button.addEventListener("click", setRate);
+});
+
+progressCover.addEventListener("click", videoPoint);
